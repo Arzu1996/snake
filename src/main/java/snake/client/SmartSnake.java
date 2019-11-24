1ec29d4f06966d8972038a165b0503b02d94c437
Arzu1996 <harzu2996@gmail.com>
2019-11-24 18:37:07 +0400

    --initial

--- src/main/java/snake/client/SmartSnake.java
+++ src/main/java/snake/client/SmartSnake.java
@@ -14,6 +14,7 @@ public class SmartSnake {
   private Point snake_head;
   private boolean gameOver;
   private ArrayList<Point> obstacles = new ArrayList<>();
+  private Direction snakeDirection;
 
   public SmartSnake(Board board) {
     List<Point> walls = board.getWalls();
@@ -28,6 +29,7 @@ public class SmartSnake {
     obstacles.add(stone);
     obstacles.addAll(snake);
     lee = new Lee(dimX, dimY);
+    snakeDirection = board.getSnakeDirection();
   }
 
   Direction solve() {
@@ -36,7 +38,38 @@ public class SmartSnake {
     // your code mus tbe here!
     Optional<List<LeePoint>> solution_apple = lee.trace(snake_head, apple, obstacles);
 
-    return Direction.UP;
+    if (apple.getX() > snake_head.getX() && apple.getY() < snake_head.getY()) {
+      if (snakeDirection == Direction.UP) return Direction.UP;
+
+      return Direction.RIGHT;
+    }
+
+    if (apple.getX() > snake_head.getX() && apple.getY() > snake_head.getY()) {
+      if (snakeDirection == Direction.DOWN) return Direction.DOWN;
+
+      return Direction.RIGHT;
+    }
+
+    if (apple.getX() < snake_head.getX() && apple.getY() > snake_head.getY()) {
+      if (snakeDirection == Direction.DOWN) return Direction.DOWN;
+      return Direction.LEFT;
+    }
+
+    if (apple.getX() < snake_head.getX() && apple.getY() < snake_head.getY()) {
+      if (snakeDirection == Direction.UP) return Direction.UP;
+
+      return Direction.LEFT;
+    }
+
+    if (apple.getX() == snake_head.getX() && apple.getY() > snake_head.getY()) return Direction.UP;
+
+    if (apple.getX() < snake_head.getX() && apple.getY() == snake_head.getY()) return Direction.RIGHT;
+
+    if (apple.getX() == snake_head.getX() && apple.getY() < snake_head.getY()) return Direction.DOWN;
+
+    if (apple.getX() > snake_head.getX() && apple.getY() == snake_head.getY()) return Direction.LEFT;
+
+    return snakeDirection;
   }
 
   private Direction coord_to_direction(Point from, LeePoint to) {
